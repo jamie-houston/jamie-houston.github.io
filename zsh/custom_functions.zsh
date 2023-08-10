fa3632bde18b6ead98ab1a6863a8f5800a848d0d
Jamie houston <jamie.houston@salesforce.com>
2023-08-10 12:34:56 -0500

    fixing gclean

--- zsh/custom_functions.zsh
+++ zsh/custom_functions.zsh
@@ -6,34 +6,34 @@
 # example >adbp hello t how are you e -> "hello <tab> how are you <enter>"
 function adbp() {
     # if they don't pass anything, just paste the clipboard
-	if [ $# -eq 0 ]
-	then
+    if [ $# -eq 0 ]
+    then
         adb shell input keyevent 279
     else
         for p in $*
-			do
-        	    case $p in
-            	    p)
-						adb shell input keyevent 279
-						;;
-	                t)
-    	                adb shell input keyevent 61
-						;;
-            	    e)
-                	    adb shell input keyevent 66
-						;;
-	                *)
-    	                adb shell input text $p
-						;;
-            	esac
-        	done
+        do
+            case $p in
+                p)
+                    adb shell input keyevent 279
+                ;;
+                t)
+                    adb shell input keyevent 61
+                ;;
+                e)
+                    adb shell input keyevent 66
+                ;;
+                *)
+                    adb shell input text $p
+                ;;
+            esac
+        done
     fi
 }
 
 # delete all branches merged into main
 gclean() {
-  echo "=== Deleting all merged branches ==="
-  git remote prune origin
-  git checkout main && git branch --merged | egrep -v "(^\*|main)" | xargs git branch -d
-  echo "☑️ Done!"
+    echo "=== Deleting all merged branches ==="
+    git remote prune origin
+    git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -df $1
+    echo "☑️ Done!"
 }
