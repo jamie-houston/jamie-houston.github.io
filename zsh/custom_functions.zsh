a29fda95b6d8d00468b1cdae21d7a5a9310f4425
Jamie Houston <jamie.houston@salesforce.com>
2023-08-14 12:51:44 -0500

    adding gradle clean

--- zsh/custom_functions.zsh
+++ zsh/custom_functions.zsh
@@ -37,3 +37,40 @@ gclean() {
     git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -df $1
     echo "☑️ Done!"
 }
+
+# Get the size of gradle caches, wrappers and daemons in human readable format
+# Depends on: du, awk
+# Use as: gradleCacheWrapperDaemonsSize
+function gradleCacheWrapperDaemonsSize(){
+    DIR_TO_CHECK=("caches"  "wrapper"  "daemon")
+    for dir in ${DIR_TO_CHECK[*]}; do
+        printf "  👉 ~/.gradle/$dir: $(du -sh ~/.gradle/$dir | awk '{ print $1 }')\n"        
+    done
+}
+
+# Clean up the gradle caches, wrappers and daemons directory of files 
+# that were accessed more than 30 days ago and remove empty directories
+# Found here: https://crushingcode.nisrulz.com/posts/reducing-mammoth-gradle-caches/
+# Depends on: gradleCacheWrapperDaemonsSize
+# Use as: gradleClean
+function gradleClean(){
+    echo " [BEFORE Cleanup] Gradle caches size:"
+    gradleCacheWrapperDaemonsSize
+    echo "=========================================================="
+    echo " Cleaning up gradle directories ..."
+    echo " "
+    echo " Working in:"
+    DIR_TO_CHECK=("caches"  "wrapper"  "daemon")
+    for dir in ${DIR_TO_CHECK[*]}; do
+        echo " 👉 ~/.gradle/$dir"
+        # Delete all files accessed 30 days ago
+        find ~/.gradle/$dir -type f -atime +30 -delete
+        # Delete empty directories
+        find ~/.gradle/$dir -mindepth 1 -type d -empty -delete
+    done
+    echo "=========================================================="
+    echo " [AFTER Cleanup] Gradle caches size:"
+    gradleCacheWrapperDaemonsSize
+    echo "=========================================================="
+    echo " Done ✅"
+}
